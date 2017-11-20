d395dae326d47efa4c7b6b2aeac1f502696a4927
eeprom01 <sean.ton@gmail.com>
2017-11-19 22:48:34 -0700

    first

---
+++ mongo.js
@@ -0,0 +1,16 @@
+const mongojs = require('mongojs');
+
+var db = mongojs('mydb', ['screenscrapertest']);
+db.screenscrapertest.insert({
+    worked:true
+});
+
+db.screenscrapertest.find({}, (err,docs) => {
+    if (err) {
+        console.log('err');
+        db.close();
+    }
+
+db.close();
+
+});
